376a91a13368c435572abfc623cb8b0ea551ff4c
TuTomasz <Ttomaszito@gmail.com>
2024-05-31 22:50:59 -0400

    example jsdoc ibrary types

--- packages/lib/types/types.d.ts
+++ packages/lib/types/types.d.ts
@@ -1,8 +1,18 @@
  
-export type Field = {
+export type Invoice = {
     id: number;
     amount: number;
-    transctions: number[];
+    transctions: Transaction[];
+    service: PaymentOption;
 }
+
+export type Transaction = {
+    id: number;
+    amount: number;
+    date: Date;
+}
+
+export const PaymentOptions = ["physical", "digital"] as const;
+export type PaymentOption = typeof PaymentOptions[number];
 
 export as namespace Types;
