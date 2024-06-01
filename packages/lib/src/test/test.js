7bcff3549853a20c9f964256a9e2b07da17d79b5
Tomasz turek <tomaszturek@Tomaszs-MBP-2.T-mobile.com>
2024-05-31 22:48:47 -0400

    example jsdoc ibrary types

--- packages/lib/src/test/test.js
+++ packages/lib/src/test/test.js
@@ -1,12 +1,25 @@
 import  Types  from "../../types/types"
  
 /** 
- * @param {Types.Field} obj - object with numbers
- * @returns  {number}
+ * @param {Types.Transaction} transction - object with numbers
+ * @returns  {Types.Invoice}
  */
-export const sumFields = (obj) => {
-     const test = obj.amount.k
-     return test
+export const getInvoice = (transction ={
+    id:1,
+    amount:10,
+    date:new Date(),
+
+}) => {
+
+     /** @type {Types.Invoice} */
+    const payload = {
+        id:1,
+        amount:10,
+        transctions:[transction],
+        service:"digital",
+     }
+
+     return payload
 }
 
 
